3e4f5eff37a8a86c8aa83642fc57c3865a86f453
unknown <abvsuxhard@gmail.com>
2022-03-06 16:39:44 +0200

    minimal ui change

--- src/components/Coin.js
+++ src/components/Coin.js
@@ -4,7 +4,7 @@ const Coin = ({name, price, icon, symbol}) => {
   return (
     <div className='coin'>
         <h1> Name: {name}</h1>
-        <img src={icon}/>
+        <img src={icon} alt='Coin icon'/>
         <h3> Price: ${price}</h3>
         <h3> Symbol: {symbol}</h3>
     </div>
